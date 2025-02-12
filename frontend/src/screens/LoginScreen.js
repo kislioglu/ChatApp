c14f2d373cbb34a4bcfbf37333503939f25a00aa
kislioglu <gkslogl@gmail.com>
2025-02-12 19:17:19 +0300

    it is now only connecting to backend when a user signed in

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,11 +1,43 @@
+import React, {useState} from 'react';
 import auth from '@react-native-firebase/auth';
-import {useState} from 'react';
-import {View, TextInput, Button, Text} from 'react-native';
+import {useEffect} from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  Text,
+  Pressable,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {connectSocket, disconnectSocket} from '../services/socket';
 
 export default function AuthScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [focusedInput, setFocusedInput] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(user => {
+      setUser(user);
+
+      if (user) {
+        connectSocket();
+        navigation.navigate('ChatScreen');
+        console.log('connected');
+      } else {
+        disconnectSocket();
+        console.log('disconnected');
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const signUp = async () => {
     try {
@@ -37,29 +69,97 @@ export default function AuthScreen() {
   };
 
   return (
-    <View>
-      {user ? (
-        <View>
-          <Text>Welcome, {user.email}</Text>
-          <Button title="Sign out" onPress={signOut} />
-        </View>
-      ) : (
-        <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View>
+        <View
+          style={[
+            styles.inputContainer,
+            focusedInput === 'email' && styles.focusedInputContainer,
+          ]}>
           <TextInput
+            style={[styles.input, {width: '100%'}]}
             placeholder="E-mail"
             onChangeText={setEmail}
             value={email}
+            onFocus={() => setFocusedInput('email')}
+            onBlur={() => setFocusedInput(null)}
           />
+        </View>
+        <View
+          style={[
+            styles.inputContainer,
+            focusedInput === 'password' && styles.focusedInputContainer,
+          ]}>
           <TextInput
-            placeholder="password"
-            secureTextEntry
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={showPassword}
             onChangeText={setPassword}
             value={password}
+            onFocus={() => setFocusedInput('password')}
+            onBlur={() => setFocusedInput(null)}
           />
-          <Button title="Signup" onPress={signUp} />
-          <Button title="Signin" onPress={signIn} />
+          <TouchableOpacity
+            style={{
+              width: 32,
+              height: 32,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={() => setShowPassword(!showPassword)}>
+            <Image source={require('../../assets/hide.png')} />
+          </TouchableOpacity>
         </View>
-      )}
+
+        <View style={{gap: 10, marginTop: 10}}>
+          <Pressable
+            style={[styles.signBtn, {backgroundColor: '#1fb141'}]}
+            onPress={signUp}>
+            <Text style={styles.signText}>Sign Up</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.signBtn, {backgroundColor: '#007bff'}]}
+            onPress={signIn}>
+            <Text style={styles.signText}>Sign In</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  signBtn: {
+    backgroundColor: 'gray',
+    height: 50,
+    padding: 10,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    padding: 10,
+    borderRadius: 5,
+    width: '90%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    height: 50,
+    marginTop: 10,
+    marginHorizontal: 10,
+  },
+  focusedInputContainer: {
+    borderColor: 'blue',
+  },
+  signText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
