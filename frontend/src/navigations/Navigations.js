6b32cbbea7fe85cc0a9a6b330dddb5da95430787
kislioglu <gkslogl@gmail.com>
2025-02-14 18:09:00 +0300

    container of screens added to navigation stack

--- frontend/src/navigations/Navigations.js
+++ frontend/src/navigations/Navigations.js
@@ -3,12 +3,18 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen';
 import ChatScreen from '../screens/ChatScreen';
+import Index from '../screens';
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Index"
+          component={Index}
+        />
         <Stack.Screen
           options={{headerShown: false}}
           name="LoginScreen"
