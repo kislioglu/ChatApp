5f18a7707ca9fa8e9c316512ce4353a5ea58303f
kislioglu <gkslogl@gmail.com>
2025-02-19 17:19:28 +0300

    added a condition for whether the chat screen should be shown or not

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,16 +1,20 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
+import React, {useState} from 'react';
 import NewUserBtn from '../components/buttons/newUserBtn';
+import ChatScreen from './ChatScreen';
 
 export default function Index() {
+  const [recipientUid, setRecipientUid] = useState('');
+
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <View style={{position: 'absolute', bottom: 20, right: 20}}>
-        <NewUserBtn />
-      </View>
+    <View style={{flex: 1}}>
+      {recipientUid ? (
+        <ChatScreen recipientUid={recipientUid} />
+      ) : (
+        <View style={{flex: 1, bottom: 20, right: 20, position: 'absolute'}}>
+          <NewUserBtn onUserSelect={setRecipientUid} />
+        </View>
+      )}
     </View>
   );
 }
